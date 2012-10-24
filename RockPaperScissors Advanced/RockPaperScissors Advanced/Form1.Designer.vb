432fae565808835f541c2a3e8175f0f3705cea31
DV8FromTheWorld <keeneraustin@yahoo.com>
2012-10-24 12:16:27 -0400

    Added RockPaperScissors - Advanced version. Will be adding a basic version later.

---
+++ RockPaperScissors Advanced/RockPaperScissors Advanced/Form1.Designer.vb
@@ -0,0 +1,114 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.grpChoices = New System.Windows.Forms.GroupBox()
+        Me.radRock = New System.Windows.Forms.RadioButton()
+        Me.radPaper = New System.Windows.Forms.RadioButton()
+        Me.radScissors = New System.Windows.Forms.RadioButton()
+        Me.lblCounterDisplay = New System.Windows.Forms.Label()
+        Me.btnGo = New System.Windows.Forms.Button()
+        Me.grpChoices.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'grpChoices
+        '
+        Me.grpChoices.Controls.Add(Me.radScissors)
+        Me.grpChoices.Controls.Add(Me.radPaper)
+        Me.grpChoices.Controls.Add(Me.radRock)
+        Me.grpChoices.Location = New System.Drawing.Point(32, 53)
+        Me.grpChoices.Name = "grpChoices"
+        Me.grpChoices.Size = New System.Drawing.Size(209, 70)
+        Me.grpChoices.TabIndex = 0
+        Me.grpChoices.TabStop = False
+        Me.grpChoices.Text = "Choose Your Throw"
+        '
+        'radRock
+        '
+        Me.radRock.AutoSize = True
+        Me.radRock.Location = New System.Drawing.Point(17, 33)
+        Me.radRock.Name = "radRock"
+        Me.radRock.Size = New System.Drawing.Size(51, 17)
+        Me.radRock.TabIndex = 0
+        Me.radRock.Text = "Rock"
+        Me.radRock.UseVisualStyleBackColor = True
+        '
+        'radPaper
+        '
+        Me.radPaper.AutoSize = True
+        Me.radPaper.Location = New System.Drawing.Point(74, 33)
+        Me.radPaper.Name = "radPaper"
+        Me.radPaper.Size = New System.Drawing.Size(53, 17)
+        Me.radPaper.TabIndex = 1
+        Me.radPaper.Text = "Paper"
+        Me.radPaper.UseVisualStyleBackColor = True
+        '
+        'radScissors
+        '
+        Me.radScissors.AutoSize = True
+        Me.radScissors.Location = New System.Drawing.Point(133, 33)
+        Me.radScissors.Name = "radScissors"
+        Me.radScissors.Size = New System.Drawing.Size(64, 17)
+        Me.radScissors.TabIndex = 2
+        Me.radScissors.Text = "Scissors"
+        Me.radScissors.UseVisualStyleBackColor = True
+        '
+        'lblCounterDisplay
+        '
+        Me.lblCounterDisplay.Location = New System.Drawing.Point(33, 139)
+        Me.lblCounterDisplay.Name = "lblCounterDisplay"
+        Me.lblCounterDisplay.Size = New System.Drawing.Size(207, 52)
+        Me.lblCounterDisplay.TabIndex = 1
+        '
+        'btnGo
+        '
+        Me.btnGo.Location = New System.Drawing.Point(83, 215)
+        Me.btnGo.Name = "btnGo"
+        Me.btnGo.Size = New System.Drawing.Size(102, 35)
+        Me.btnGo.TabIndex = 2
+        Me.btnGo.Text = "Go!"
+        Me.btnGo.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.Controls.Add(Me.btnGo)
+        Me.Controls.Add(Me.lblCounterDisplay)
+        Me.Controls.Add(Me.grpChoices)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.grpChoices.ResumeLayout(False)
+        Me.grpChoices.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents grpChoices As System.Windows.Forms.GroupBox
+    Friend WithEvents radScissors As System.Windows.Forms.RadioButton
+    Friend WithEvents radPaper As System.Windows.Forms.RadioButton
+    Friend WithEvents radRock As System.Windows.Forms.RadioButton
+    Friend WithEvents lblCounterDisplay As System.Windows.Forms.Label
+    Friend WithEvents btnGo As System.Windows.Forms.Button
+
+End Class
---
+++ RockPaperScissors Advanced/RockPaperScissors Advanced/Form1.vb
@@ -0,0 +1,109 @@
+﻿Public Class Form1
+    'Austin Keener
+    'RockPaperScissors - Advanced version
+    '10/24/2012
+
+    'Defining global variables and initializing them for use.
+    Dim intComputerChoice As Integer = 0
+    Dim intWins As Integer = 0
+    Dim intLosses As Integer = 0
+    Dim intTies As Integer = 0
+
+    'Needed for the use of Generate.Next(1, 4) to generate a random number for the computer's choice.
+    Dim Generate As New Random()
+
+    'Executed when the program FIRST starts.  This is needed so that the computer's choice is selected from the very beginning.
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        updatecomputerChoice()
+    End Sub
+
+    'Executed when the button is clicked.
+    Private Sub btnGo_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnGo.Click
+        '1 represents Rock, 2 represents Paper, 3 represents Scissors
+
+        If radRock.Checked Then          'Checks if the user has the "Rock" choice selected
+            Select Case intComputerChoice       'Selects the variable "intComputerChoice" to match cases against
+                Case 1
+                    intTies += 1
+                    displayResult("Rock", "Rock", "tied.")      'For information on this method/code, please check the method at the bottom for an indepth explination.
+                Case 2
+                    intLosses += 1
+                    displayResult("Rock", "Paper", "lost.")
+                Case 3
+                    intWins += 1
+                    displayResult("Rock", "Scissors", "won!")
+                Case Else
+                    MessageBox.Show("Error, something went wrong with the computer generated choice")   'Not nessesarily needed, just here to catch errors
+            End Select
+
+        ElseIf radPaper.Checked Then         'Checks if the user has the "Paper" choice selected
+            Select Case intComputerChoice       'Selects the variable "intComputerChoice" to match cases against
+                Case 1
+                    intWins += 1
+                    displayResult("Paper", "Rock", "won!")
+                Case 2
+                    intTies += 1
+                    displayResult("Paper", "Paper", "tied.")
+                Case 3
+                    intLosses += 1
+                    displayResult("Paper", "Scissors", "lost.")
+                Case Else
+                    MessageBox.Show("Error, something went wrong with the computer generated choice")   'Not nessesarily needed, just here to catch errors
+            End Select
+
+        ElseIf radScissors.Checked Then     'Checks if the user has the "Rock" choice selected
+            Select Case intComputerChoice       'Selects the variable "intComputerChoice" to match cases against
+                Case 1
+                    intLosses += 1
+                    displayResult("Scissors", "Rock", "lost.")
+                Case 2
+                    intWins += 1
+                    displayResult("Scissors", "Paper", "won!")
+                Case 3
+                    intTies += 1
+                    displayResult("Scissors", "Scissors", "tied.")
+                Case Else
+                    MessageBox.Show("Error, something went wrong with the computer generated choice")   'Not nessesarily needed, just here to catch errors
+            End Select
+        Else
+            MessageBox.Show("No choice selected, Please select one of the 3 choice to play")    'Shown when none of the Radio buttons have been selected.
+        End If
+
+        'These methods are called to change the computer's choice for the next round and to update the scoreboard.
+        'For information on each specific method, check the comments and code relating to the methods.
+        updateScoreBoard()
+        updatecomputerChoice()
+
+    End Sub
+
+    'This method updates the computers choice.
+    Public Sub updatecomputerChoice()
+
+        'This code generates a random integer between 1 and 4, but 4 is not inclusive, meaning the only possibilities are 1, 2 and 3
+        '1 represents Rock, 2 represents Paper, 3 represents Scissors
+        intComputerChoice = Generate.Next(1, 4)
+    End Sub
+
+    'This method updates the label with the correct scores.
+    Public Sub updateScoreBoard()
+        lblCounterDisplay.Text = _
+            "Wins: " & intWins & vbCrLf & _
+            "Losses: " & intLosses & vbCrLf & _
+            "Ties: " & intTies
+    End Sub
+
+
+    'This method displays the results of each round in a MessageBox
+    'This method is useful so that you do not need to have the code for a message box in every case in the case selects above.  It is more efficent this way.
+    'When you call this method, you need to provide 3 strings.
+    'An example of how to use this method is as follows:   displayResult("cat", "jimmy", "house")
+    'This would populate the variable "playerChoice" with the string: "cat"
+    'This would populate the variable "computerChoice" with the string: "jimmy"
+    'This would populate the variable"house" with the string: "house
+    'So, when the "displayResult("cat", "jimmy", "house") is executed, it will populate those variables and run the code in the method, 
+    'Cause the messagebox to display the following:   "Your choice: cat,  Computer choice: jimmy,  You house"
+    Public Sub displayResult(ByVal playerChoice As String, ByVal computerChoice As String, ByVal result As String)
+
+        MessageBox.Show("Your choice: " & playerChoice & ",  Computer choice: " & computerChoice & ",  You " & result)
+    End Sub
+End Class
