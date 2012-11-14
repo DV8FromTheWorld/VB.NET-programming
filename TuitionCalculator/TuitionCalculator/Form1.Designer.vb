d1ec47748a56a1c3e1703d2edfd2a1a8b9e8824f
DV8FromTheWorld <keeneraustin@yahoo.com>
2012-11-14 12:59:48 -0500

    Added Tuition Calculator

---
+++ TuitionCalculator/TuitionCalculator/Form1.Designer.vb
@@ -0,0 +1,109 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblPrompt = New System.Windows.Forms.Label()
+        Me.lblDisplay = New System.Windows.Forms.Label()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.lstDegrees = New System.Windows.Forms.ListBox()
+        Me.lblPromptV2 = New System.Windows.Forms.Label()
+        Me.cboHours = New System.Windows.Forms.ComboBox()
+        Me.SuspendLayout()
+        '
+        'lblPrompt
+        '
+        Me.lblPrompt.AutoSize = True
+        Me.lblPrompt.Location = New System.Drawing.Point(25, 17)
+        Me.lblPrompt.Name = "lblPrompt"
+        Me.lblPrompt.Size = New System.Drawing.Size(72, 13)
+        Me.lblPrompt.TabIndex = 0
+        Me.lblPrompt.Text = "Course Level:"
+        '
+        'lblDisplay
+        '
+        Me.lblDisplay.Location = New System.Drawing.Point(25, 101)
+        Me.lblDisplay.Name = "lblDisplay"
+        Me.lblDisplay.Size = New System.Drawing.Size(247, 23)
+        Me.lblDisplay.TabIndex = 1
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.Location = New System.Drawing.Point(179, 66)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(93, 23)
+        Me.btnCalculate.TabIndex = 2
+        Me.btnCalculate.Text = "Calculate"
+        Me.btnCalculate.UseVisualStyleBackColor = True
+        '
+        'lstDegrees
+        '
+        Me.lstDegrees.FormattingEnabled = True
+        Me.lstDegrees.Items.AddRange(New Object() {"Undergraduate", "Graduate", "Thesis and Dissertation"})
+        Me.lstDegrees.Location = New System.Drawing.Point(28, 33)
+        Me.lstDegrees.Name = "lstDegrees"
+        Me.lstDegrees.Size = New System.Drawing.Size(135, 56)
+        Me.lstDegrees.TabIndex = 3
+        '
+        'lblPromptV2
+        '
+        Me.lblPromptV2.AutoSize = True
+        Me.lblPromptV2.Location = New System.Drawing.Point(181, 17)
+        Me.lblPromptV2.Name = "lblPromptV2"
+        Me.lblPromptV2.Size = New System.Drawing.Size(65, 13)
+        Me.lblPromptV2.TabIndex = 5
+        Me.lblPromptV2.Text = "Credit Hours"
+        '
+        'cboHours
+        '
+        Me.cboHours.FormattingEnabled = True
+        Me.cboHours.Items.AddRange(New Object() {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18"})
+        Me.cboHours.Location = New System.Drawing.Point(181, 37)
+        Me.cboHours.Name = "cboHours"
+        Me.cboHours.Size = New System.Drawing.Size(64, 21)
+        Me.cboHours.TabIndex = 6
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 144)
+        Me.Controls.Add(Me.cboHours)
+        Me.Controls.Add(Me.lblPromptV2)
+        Me.Controls.Add(Me.lstDegrees)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Controls.Add(Me.lblDisplay)
+        Me.Controls.Add(Me.lblPrompt)
+        Me.Name = "Form1"
+        Me.Text = "Tuition Calculator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents lblPrompt As System.Windows.Forms.Label
+    Friend WithEvents lblDisplay As System.Windows.Forms.Label
+    Friend WithEvents btnCalculate As System.Windows.Forms.Button
+    Friend WithEvents lstDegrees As System.Windows.Forms.ListBox
+    Friend WithEvents lblPromptV2 As System.Windows.Forms.Label
+    Friend WithEvents cboHours As System.Windows.Forms.ComboBox
+
+End Class
---
+++ TuitionCalculator/TuitionCalculator/Form1.vb
@@ -0,0 +1,50 @@
+﻿Public Class Form1
+    'Austin Keener
+    '11/14/2012
+    'Tuition Calculator
+
+    'Initialize required variables
+    Const intUndergraduatePrice As Integer = 75     'Constants used because these prices never change.
+    Const intGraduatePrice As Integer = 145
+    Const intThesisPrice As Integer = 165
+    Dim intHours As Integer = 0
+    Dim dblTuition As Double = 0.0
+
+    'Executed when the Calulcate button is clicked.
+    Private Sub btnCalculate_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnCalculate.Click
+        Select Case lstDegrees.SelectedIndex
+            Case -1                     'Case for nothing selected
+                dblTuition = 0.0            'Set to 0 for boolean check later
+            Case 0                      'Case for Undergraduate Selected
+                dblTuition = intUndergraduatePrice
+            Case 1                      'Case for Graduate Selected
+                dblTuition = intGraduatePrice
+            Case 2                      'Case for Thesis and Dissertation selected
+                dblTuition = intThesisPrice
+        End Select
+
+        If Not dblTuition.Equals(0.0) Then      'Checks that a course level is selected based on the Case Select code above
+            If Not intHours.Equals(0) Then      'Checks that an amount of credit hours has been specified
+                lblDisplay.Text = "Tuition is " & (intHours * dblTuition).ToString("$##0.00")
+            Else
+                MessageBox.Show("Please selected an amount of credit hours")
+            End If
+        Else
+            MessageBox.Show("Please select a course level")
+        End If
+    End Sub
+
+    'Exectues when the selected value is changed in the Credit Hours combo-box
+    Private Sub cboHours_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cboHours.SelectedIndexChanged
+        Try                 'Try catch to prevent exceptions(errors)
+            intHours = cboHours.Text
+        Catch ex As Exception
+            intHours = 0        'Set to 0 for boolean check
+        End Try
+    End Sub
+
+    'Exectued when the program first loads
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        cboHours.Text = 0       'Sets the number shown in the Credit Hours box to be 0 for show. (not required, just for looks)
+    End Sub
+End Class
