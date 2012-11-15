55c42623e1b06f8be5544d32bc88cb4956479496
DV8FromTheWorld <keeneraustin@yahoo.com>
2012-11-15 12:24:32 -0500

    added LunchTime

---
+++ LunchTime/LunchTime/Form1.Designer.vb
@@ -0,0 +1,166 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.mnuMain = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuClose = New System.Windows.Forms.ToolStripMenuItem()
+        Me.lstChoices = New System.Windows.Forms.ListBox()
+        Me.lblChoicePrompt = New System.Windows.Forms.Label()
+        Me.cboDrinks = New System.Windows.Forms.ComboBox()
+        Me.lblDrinkPrompt = New System.Windows.Forms.Label()
+        Me.cboSize = New System.Windows.Forms.ComboBox()
+        Me.lblSizePrompt = New System.Windows.Forms.Label()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.lblDisplay = New System.Windows.Forms.Label()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.mnuMain})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(284, 24)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'mnuMain
+        '
+        Me.mnuMain.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.mnuClose})
+        Me.mnuMain.Name = "mnuMain"
+        Me.mnuMain.Size = New System.Drawing.Size(46, 20)
+        Me.mnuMain.Text = "Main"
+        '
+        'mnuClose
+        '
+        Me.mnuClose.Name = "mnuClose"
+        Me.mnuClose.Size = New System.Drawing.Size(103, 22)
+        Me.mnuClose.Text = "Close"
+        '
+        'lstChoices
+        '
+        Me.lstChoices.FormattingEnabled = True
+        Me.lstChoices.Items.AddRange(New Object() {"Chef Salad", "Garden Salad", "Hamburger", "Cheeseburger", "Chicken Sandwich"})
+        Me.lstChoices.Location = New System.Drawing.Point(12, 59)
+        Me.lstChoices.Name = "lstChoices"
+        Me.lstChoices.Size = New System.Drawing.Size(123, 108)
+        Me.lstChoices.TabIndex = 1
+        '
+        'lblChoicePrompt
+        '
+        Me.lblChoicePrompt.AutoSize = True
+        Me.lblChoicePrompt.Location = New System.Drawing.Point(12, 43)
+        Me.lblChoicePrompt.Name = "lblChoicePrompt"
+        Me.lblChoicePrompt.Size = New System.Drawing.Size(85, 13)
+        Me.lblChoicePrompt.TabIndex = 2
+        Me.lblChoicePrompt.Text = "Today's Choices"
+        '
+        'cboDrinks
+        '
+        Me.cboDrinks.FormattingEnabled = True
+        Me.cboDrinks.Items.AddRange(New Object() {"Sprite", "Coke", "Sundrop", "Unsweet Tea", "Sweet Tea", "Lemonade"})
+        Me.cboDrinks.Location = New System.Drawing.Point(156, 59)
+        Me.cboDrinks.Name = "cboDrinks"
+        Me.cboDrinks.Size = New System.Drawing.Size(116, 21)
+        Me.cboDrinks.TabIndex = 3
+        '
+        'lblDrinkPrompt
+        '
+        Me.lblDrinkPrompt.AutoSize = True
+        Me.lblDrinkPrompt.Location = New System.Drawing.Point(169, 43)
+        Me.lblDrinkPrompt.Name = "lblDrinkPrompt"
+        Me.lblDrinkPrompt.Size = New System.Drawing.Size(37, 13)
+        Me.lblDrinkPrompt.TabIndex = 4
+        Me.lblDrinkPrompt.Text = "Drinks"
+        '
+        'cboSize
+        '
+        Me.cboSize.FormattingEnabled = True
+        Me.cboSize.Items.AddRange(New Object() {"Small", "Medium", "Large"})
+        Me.cboSize.Location = New System.Drawing.Point(156, 108)
+        Me.cboSize.Name = "cboSize"
+        Me.cboSize.Size = New System.Drawing.Size(116, 21)
+        Me.cboSize.TabIndex = 5
+        '
+        'lblSizePrompt
+        '
+        Me.lblSizePrompt.AutoSize = True
+        Me.lblSizePrompt.Location = New System.Drawing.Point(169, 92)
+        Me.lblSizePrompt.Name = "lblSizePrompt"
+        Me.lblSizePrompt.Size = New System.Drawing.Size(55, 13)
+        Me.lblSizePrompt.TabIndex = 6
+        Me.lblSizePrompt.Text = "Drink Size"
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.Location = New System.Drawing.Point(156, 148)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(116, 27)
+        Me.btnCalculate.TabIndex = 7
+        Me.btnCalculate.Text = "Calculate"
+        Me.btnCalculate.UseVisualStyleBackColor = True
+        '
+        'lblDisplay
+        '
+        Me.lblDisplay.Location = New System.Drawing.Point(19, 187)
+        Me.lblDisplay.Name = "lblDisplay"
+        Me.lblDisplay.Size = New System.Drawing.Size(252, 64)
+        Me.lblDisplay.TabIndex = 8
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.Controls.Add(Me.lblDisplay)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Controls.Add(Me.lblSizePrompt)
+        Me.Controls.Add(Me.cboSize)
+        Me.Controls.Add(Me.lblDrinkPrompt)
+        Me.Controls.Add(Me.cboDrinks)
+        Me.Controls.Add(Me.lblChoicePrompt)
+        Me.Controls.Add(Me.lstChoices)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.Text = "LunchTime"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents MenuStrip1 As System.Windows.Forms.MenuStrip
+    Friend WithEvents mnuMain As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents mnuClose As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents lstChoices As System.Windows.Forms.ListBox
+    Friend WithEvents lblChoicePrompt As System.Windows.Forms.Label
+    Friend WithEvents cboDrinks As System.Windows.Forms.ComboBox
+    Friend WithEvents lblDrinkPrompt As System.Windows.Forms.Label
+    Friend WithEvents cboSize As System.Windows.Forms.ComboBox
+    Friend WithEvents lblSizePrompt As System.Windows.Forms.Label
+    Friend WithEvents btnCalculate As System.Windows.Forms.Button
+    Friend WithEvents lblDisplay As System.Windows.Forms.Label
+
+End Class
---
+++ LunchTime/LunchTime/Form1.vb
@@ -0,0 +1,99 @@
+﻿Public Class Form1
+    'Austin Keener
+    '11/15/2012
+    'Lunch Time
+
+    'Defines and initalizes the variables required by the program
+    Dim strChoice As String = ""
+    Dim strDrink As String = ""
+    Dim strDrinkSize As String = ""
+    Dim dblSizePrice As Double = 0.0
+    Dim dblChoicePrice As Double = 0.0
+
+    'Executed when the user changes the selected Drink
+    Private Sub cboDrinks_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cboDrinks.SelectedIndexChanged
+        Try
+            strDrink = cboDrinks.Text
+        Catch ex As Exception
+            strChoice = ""          'Set to empty for boolean check in the Calculate button method
+        End Try
+    End Sub
+
+    'Executed when the user changes the selected Drink Size
+    Private Sub cboSize_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cboSize.SelectedIndexChanged
+        Select Case cboSize.Text                '
+            Case "Small"
+                strDrinkSize = "Small"
+                dblSizePrice = 0.99
+            Case "Medium"
+                strDrinkSize = "Medium"
+                dblSizePrice = 1.49
+            Case "Large"
+                strDrinkSize = "Large"
+                dblSizePrice = 1.99
+            Case Else                   'Catches any mistakes.
+                strDrinkSize = ""       'Set to empty for boolean check in the calculate buttom method
+                dblSizePrice = 0.0      'Set to 0.0 for boolean check in the calculate buttom method
+        End Select
+    End Sub
+
+    'Executed when the Calculate button is clicked
+    Private Sub btnCalculate_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnCalculate.Click
+        If Not dblChoicePrice.Equals(0.0) And Not String.IsNullOrEmpty(strChoice) Then  'Checks that the a Meal Choice has been selected
+            If Not String.IsNullOrEmpty(strDrink) Then                                  'Checks that a Drink has been selected
+                If Not dblSizePrice.Equals(0.0) And Not String.IsNullOrEmpty(strDrinkSize) Then 'Checks that a Drink Size has been selected
+                    lblDisplay.Text = _
+                        "You ordered: " & strChoice & vbCrLf & _
+                        "Your drink is: " & strDrink & vbCrLf & _
+                        "Drink Size: " & strDrinkSize & vbCrLf & _
+                        "Total Cost: " & ((dblChoicePrice + dblSizePrice) * 1.07).ToString("$##0.00")
+
+                Else
+                    MessageBox.Show("Please choose a drink size")           'Informs the user of their mistake in ordering
+                End If
+            Else
+                MessageBox.Show("Please choose a drink")                    'Informs the user of their mistake in ordering
+            End If
+        Else
+            MessageBox.Show("Please choose a meal from the choices listed") 'Informs the user of their mistake in ordering
+        End If
+    End Sub
+
+    'Executed when the Close option is selected in the main menu
+    Private Sub mnuClose_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles mnuClose.Click
+        Application.Exit()              'Closes the program
+    End Sub
+
+    'Executed when the program first starts.
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        cboSize.Text = "Chose a drink size"         'Sets the text displayed in the Drink Size combo box on startup (just for show, not required)
+        cboDrinks.Text = "Chose a drink"            'Sets the text displayed in the Drink combo box on startup (just for show, not required
+    End Sub
+
+    'Executes when the user changes the selected Choice of Meal
+    Private Sub lstChoices_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles lstChoices.SelectedIndexChanged
+        Select Case lstChoices.SelectedIndex
+            Case -1
+                strChoice = ""          'Set to empty for boolean check in the Calculate button method
+                dblChoicePrice = 0.0    'Set to 0.0 for boolean check in the Calculate button method
+            Case 0
+                strChoice = "Chef Salad"
+                dblChoicePrice = 3.99
+                MessageBox.Show("We only use the freshest ingredients in our salads!")
+            Case 1
+                strChoice = "Garden Salad"
+                dblChoicePrice = 2.99
+                MessageBox.Show("We only use the freshest ingredients in our salads!")
+            Case 2
+                strChoice = "Hamburger"
+                dblChoicePrice = "2.99"
+            Case 3
+                strChoice = "Cheeseburger"
+                dblChoicePrice = 3.49
+            Case 4
+                strChoice = "Chicken Sandwich"
+                dblChoicePrice = 3.99
+        End Select
+    End Sub
+
+End Class
