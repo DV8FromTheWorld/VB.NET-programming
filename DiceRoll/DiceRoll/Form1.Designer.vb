0f99296837ad15a10285c22e46158c9069a43d98
DV8FromTheWorld <keeneraustin@yahoo.com>
2013-01-14 15:15:06 -0500

    Final commit for this repo.  Class completed today. [1/14/2013]

---
+++ DiceRoll/DiceRoll/Form1.Designer.vb
@@ -0,0 +1,112 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.pic1 = New System.Windows.Forms.PictureBox()
+        Me.pic2 = New System.Windows.Forms.PictureBox()
+        Me.pic3 = New System.Windows.Forms.PictureBox()
+        Me.btnStart = New System.Windows.Forms.Button()
+        Me.btnStop = New System.Windows.Forms.Button()
+        Me.tmrInterval = New System.Windows.Forms.Timer(Me.components)
+        CType(Me.pic1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pic2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pic3, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'pic1
+        '
+        Me.pic1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.pic1.Location = New System.Drawing.Point(39, 43)
+        Me.pic1.Name = "pic1"
+        Me.pic1.Size = New System.Drawing.Size(150, 150)
+        Me.pic1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.pic1.TabIndex = 0
+        Me.pic1.TabStop = False
+        '
+        'pic2
+        '
+        Me.pic2.Location = New System.Drawing.Point(296, 43)
+        Me.pic2.Name = "pic2"
+        Me.pic2.Size = New System.Drawing.Size(150, 150)
+        Me.pic2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.pic2.TabIndex = 1
+        Me.pic2.TabStop = False
+        '
+        'pic3
+        '
+        Me.pic3.Location = New System.Drawing.Point(553, 43)
+        Me.pic3.Name = "pic3"
+        Me.pic3.Size = New System.Drawing.Size(150, 150)
+        Me.pic3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.pic3.TabIndex = 2
+        Me.pic3.TabStop = False
+        '
+        'btnStart
+        '
+        Me.btnStart.Location = New System.Drawing.Point(195, 106)
+        Me.btnStart.Name = "btnStart"
+        Me.btnStart.Size = New System.Drawing.Size(95, 29)
+        Me.btnStart.TabIndex = 3
+        Me.btnStart.Text = "Start"
+        Me.btnStart.UseVisualStyleBackColor = True
+        '
+        'btnStop
+        '
+        Me.btnStop.Location = New System.Drawing.Point(452, 106)
+        Me.btnStop.Name = "btnStop"
+        Me.btnStop.Size = New System.Drawing.Size(95, 29)
+        Me.btnStop.TabIndex = 4
+        Me.btnStop.Text = "Stop"
+        Me.btnStop.UseVisualStyleBackColor = True
+        '
+        'tmrInterval
+        '
+        Me.tmrInterval.Interval = 250
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(751, 262)
+        Me.Controls.Add(Me.btnStop)
+        Me.Controls.Add(Me.btnStart)
+        Me.Controls.Add(Me.pic3)
+        Me.Controls.Add(Me.pic2)
+        Me.Controls.Add(Me.pic1)
+        Me.Name = "Form1"
+        Me.Text = "Dice Roll"
+        CType(Me.pic1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pic2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pic3, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents pic1 As System.Windows.Forms.PictureBox
+    Friend WithEvents pic2 As System.Windows.Forms.PictureBox
+    Friend WithEvents pic3 As System.Windows.Forms.PictureBox
+    Friend WithEvents btnStart As System.Windows.Forms.Button
+    Friend WithEvents btnStop As System.Windows.Forms.Button
+    Friend WithEvents tmrInterval As System.Windows.Forms.Timer
+
+End Class
---
+++ DiceRoll/DiceRoll/Form1.vb
@@ -0,0 +1,38 @@
+﻿Public Class Form1
+    'Austin Keener
+    '1/10/2013
+    'Dice Roll
+
+    Dim Gen As New Random()
+
+    Private Sub btnStart_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnStart.Click
+        tmrInterval.Start()
+    End Sub
+
+    Private Sub btnStop_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnStop.Click
+        tmrInterval.Stop()
+    End Sub
+
+    Private Sub tmrInterval_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles tmrInterval.Tick
+        imageChange(Gen.Next(1, 7), pic1)
+        imageChange(Gen.Next(1, 7), pic2)
+        imageChange(Gen.Next(1, 7), pic3)
+    End Sub
+
+    Private Sub imageChange(ByVal dblRand As Double, ByRef picBox As PictureBox)
+        Select Case dblRand
+            Case 1
+                picBox.Image = My.Resources.die_face_1
+            Case 2
+                picBox.Image = My.Resources.die_face_2
+            Case 3
+                picBox.Image = My.Resources.die_face_3
+            Case 4
+                picBox.Image = My.Resources.die_face_4
+            Case 5
+                picBox.Image = My.Resources.die_face_5
+            Case 6
+                picBox.Image = My.Resources.die_face_6
+        End Select
+    End Sub
+End Class
